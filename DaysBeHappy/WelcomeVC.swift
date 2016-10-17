0344bff2a30fdfcf013b18632221d0a82e8a70eb
Dan Lindsay <dlindsay72@hotmail.com>
2016-10-17 17:09:09 -0400

    Basic setup done, WelcomeVC basic UI done

--- DaysBeHappy/WelcomeVC.swift
+++ DaysBeHappy/WelcomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomVC.swift
 //  DaysBeHappy
 //
 //  Created by Dan Lindsay on 2016-10-17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
