a31d8890a85f3e1a5d1dfbebfed570c00eff900b
Dan Lindsay <dlindsay72@hotmail.com>
2016-10-18 11:21:51 -0400

    Can now add images from camera roll to our CollectionView, need to figure out where the search bar has vanished off too

--- DaysBeHappy/MemoriesVC.swift
+++ DaysBeHappy/MemoriesVC.swift
@@ -11,15 +11,17 @@ import Photos
 import AVFoundation
 import Speech
 
-class MemoriesVC: UICollectionViewController {
+class MemoriesVC: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDelegateFlowLayout {
     
     
-    
+    var memories = [URL]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
 
-        // Do any additional setup after loading the view.
+        loadMemories()
     }
 
     func checkPermissions() {
@@ -42,5 +44,189 @@ class MemoriesVC: UICollectionViewController {
         
         checkPermissions()
     }
+    
+    func getDocumentsDirectory() -> URL {
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        
+        return documentsDirectory
+    }
+    
+    func loadMemories() {
+        
+        memories.removeAll()
+        
+        // attempt to load all memories in the documents directory
+        
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else { return }
+        
+        // loop over every file found
+        for file in files {
+            let filename = file.lastPathComponent
+            
+            //check it ends with ".thumb" so we don't count each memory more than once
+            if filename.hasSuffix(".thumb") {
+                
+                //get the root name of the memory (i.e., without it's path extension)
+                let noExtension = filename.replacingOccurrences(of: ".thumb", with: "")
+                
+                //create a full apth from the memory
+                let memoryPath = getDocumentsDirectory().appendingPathComponent(noExtension)
+                
+                //add it to our array
+                memories.append(memoryPath)
+            }
+        }
+        //reload out list of memories
+        collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+    
+    func addTapped() {
+        let vc = UIImagePickerController()
+        vc.modalPresentationStyle = .formSheet
+        vc.delegate = self
+        navigationController?.present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true)
+        
+        if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            saveNewMemory(image: possibleImage)
+            loadMemories()
+        }
+    }
+    
+    func saveNewMemory(image: UIImage) {
+        
+        let memoryName = "memory-\(Date().timeIntervalSince1970)"
+        
+        let imageName = memoryName + ".jpg"
+        let thumbnailName = memoryName + ".thumb"
+        
+        do {
+            let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+            
+            if let jpegData = UIImageJPEGRepresentation(image, 80) {
+                try jpegData.write(to: imagePath, options: [.atomicWrite])
+            }
+            
+            if let thumbnail = resize(image: image, to: 200) {
+                let imagePath = getDocumentsDirectory().appendingPathComponent(thumbnailName)
+                
+                if let jpegData = UIImageJPEGRepresentation(thumbnail, 80) {
+                    try jpegData.write(to: imagePath, options: [.atomicWrite])
+                }
+                
+            }
+            
+        } catch {
+            print("Failed to save to disk")
+        }
+    }
+    
+    func resize(image: UIImage, to width: CGFloat) -> UIImage? {
+        
+        let scale = width / image.size.width
+        
+        let height = image.size.height * scale
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        
+        image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if section == 0 {
+            return 0
+        } else {
+            return memories.count
+        }
+    }
+    
+    func imageURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("jpg")
+    }
+    
+    func thumbnailURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("thumb")
+    }
+    
+    func audioURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("m4a")
+    }
+    
+    func transcriptionURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("txt")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Memory", for: indexPath) as! MemoryCell
+        
+        let memory = memories[indexPath.row]
+        let imageName = thumbnailURL(for: memory).path
+        let image = UIImage.init(contentsOfFile: imageName)
+        
+        cell.imageView.image = image
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+       return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Header", for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        if section == 1 {
+            return CGSize.zero
+        } else {
+            return CGSize(width: 0, height: 0)
+        }
+    }
+        
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
